9112717bc4cd17107b1a2c9a041440dfeba2fc84
Dominic Grieco <dominic837@gmail.com>
2020-01-22 19:57:30 -0400

    update backup directoy final

--- menu/functions/serverid.sh
+++ menu/functions/serverid.sh
@@ -74,7 +74,7 @@ recreatefolder() {
 conf="/opt/appdata/plexguide/rclone.conf"
 if [[ -e "$conf" ]]; then
         start=$(cat /var/plexguide/server.id)
-        serveridcreate=$(tree -d -L 1 /mnt/gdrive/backup | awk '{print $2}' | tail -n +2 | head -n -2 | grep "$(cat /var/plexguide/server.id)")
+        serveridcreate=$(tree -d -L 1 /mnt/gdrive/systems | awk '{print $2}' | tail -n +2 | head -n -2 | grep "$(cat /var/plexguide/server.id)")
         if [[ $start != $serveridcreate ]]; then
         rclone mkdir gdrive:/backup/$(cat /var/plexguide/server.id) --config /opt/appdata/plexguide/rclone.conf;fi
 tee <<-EOF
